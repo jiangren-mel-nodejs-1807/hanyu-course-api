8feaf1cb34e41a2688658cb1752f1227153aad45
Cheng Chang <tonychangcheng@gmail.com>
2018-09-11 20:14:13 +1000

    check the existence of the enrollment before create

--- server/models/enrollment.js
+++ server/models/enrollment.js
@@ -1,5 +1,19 @@
 'use strict';
 
 module.exports = function(Enrollment) {
-
+    // check the existence of the new enrollment
+    Enrollment.beforeRemote('create', function(context, user, next) {
+        Enrollment.findOne({
+            where: {
+                courseId: context.req.body.courseId,
+                studentId: context.req.body.studentId
+            }
+        }, function(err, results) {
+            if (err || results) {
+                context.res.status(400).send("enrollment existed");
+            } else {
+                next();
+            }
+        })
+    })
 };
